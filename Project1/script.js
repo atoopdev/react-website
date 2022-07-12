6aa3238930ba2e471472bbb40644f7b16d85952d
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-07-12 14:57:26 -0700

    Basic project file setup

---
+++ Project1/script.js
@@ -0,0 +1,38 @@
+function Page(){
+    return <div>
+        <Header /> 
+        <ListContent />
+    <Footer />
+</div>
+} 
+
+function Header(){
+    return <header>
+        <nav className="nav">
+        <img className="nav-logo" src="./react-logo.png"></img>
+        <h1>Nav</h1>
+        </nav>
+    </header>
+}
+
+function ListContent(){
+    return <div className="listContent">
+                <h1>Fun facts about React</h1>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps.</li>
+    </ul>
+    </div>
+}
+
+function Footer(){
+    return <footer className="footer">&copy; 2022 Toop development. All rights reserved.</footer>
+}
+
+// functionmust be wrapped in </>
+ReactDOM.render(<div>
+    <Page />
+    </div>, document.getElementById("root"))
